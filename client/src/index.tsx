101a5d94fe4d85dafa8da0e746443ef284c0d032
Tao Ning <tao.ning>
2022-04-11 22:55:20 -0700

    a little polish

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {CommitFeedList} from "./components/CommitFeedList";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path=':userName/:repoName' element={<CommitFeedList />} />
+              <Route path='*' element={
+                  <main style={{ display: 'flex', justifyContent: 'center', fontSize: '35px', alignItems: 'center', height: '100vh'}}>
+                      <strong >The Repo doesn't exist</strong>
+                  </main>
+              } />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
